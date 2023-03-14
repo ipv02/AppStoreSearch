064d0cf9157a8341fd72bdf243a3c6b569ace54a
Igor P-V <ispogiba@gmail.com>
2023-03-14 10:23:45 +0300

    Initial Commit

---
+++ AppStoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreSearch
+//
+//  Created by Igor P-V on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
